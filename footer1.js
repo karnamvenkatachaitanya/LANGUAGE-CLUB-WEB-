8eb4fbc6423a45529ab0f5584e9693c9f832d97b
karnamvenkatachaitanya <23kb1a3037@nbkrist.org>
2025-03-27 09:31:29 +0530

    Create footer1.js

---
+++ footer1.js
@@ -0,0 +1,9 @@
+// Back to Top functionality
+const backToTopButton = document.getElementById('backToTop');
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 300) {
+    backToTopButton.classList.add('show');
+  } else {
+    backToTopButton.classList.remove('show');
+  }
+});
